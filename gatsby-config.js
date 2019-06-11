6af3019877cffea6a5b8d8f2b30d142f85fdec3d
Eyal Roth <eyalroth1@gmail.com>
2019-06-11 08:21:09 +0300

    Add site URL as an environment variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,17 @@
+let activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
+
+console.log(`Using environment config: '${activeEnv}'`)
+
+require("dotenv").config({
+  path: `.env.${activeEnv}`,
+})
+
 const lost = require('lost')
 const pxtorem = require('postcss-pxtorem')
 
 module.exports = {
   siteMetadata: {
-    url: 'https://lumen.netlify.com',
+    url: process.env.URL,
     title: 'Blog by John Doe',
     subtitle:
       'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
