80c5078de5a67b1f5dbcb2fdc2715d667943a28e
Eyal Roth <eyalroth1@gmail.com>
2019-06-13 14:17:45 +0300

    Configure code highlighting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,6 @@
+require("prismjs/themes/prism-okaidia.css")
+require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+
 export const onServiceWorkerUpdateReady = () => {
     const answer = window.confirm(
       `This application has been updated. ` +
