ac8eb21b084b3bbd8f58e3bd60139c98a4972021
Eyal Roth <eyalroth1@gmail.com>
2019-07-08 13:42:58 +0300

    Fix bug with the sidebar menu transitioning when clicking on same page link (it shouldn't transition)

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -181,7 +181,8 @@ class Sidebar extends React.Component {
         const { left, width } = link.getBoundingClientRect()
         underline.current.setStyle({
           left,
-          width
+          width,
+          transition: "none"
         })
       }
     }
