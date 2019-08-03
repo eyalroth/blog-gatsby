a7d1cc080a1af982ed6509d777ade46cfd01ea37
Eyal Roth <eyalroth1@gmail.com>
2019-08-03 14:02:47 +0300

    Fix build following the context refactor

--- src/components/CategoryTab/index.jsx
+++ src/components/CategoryTab/index.jsx
@@ -13,19 +13,24 @@ class CategoryTab extends React.Component {
                 { ({language}) => {
                     const languageId = language.get().id
                     const category = Object.values(CategoryLinks[languageId]).find(link => link.id == categoryId)
-                    return (
-                        <div className="category-tab">
-                            <div className="category-tab-box">
-                                <Link className="category-tab-link" to={category.path}>
-                                    {(category.icon && language.ltr) ? <i className={category.icon} /> : null}
-                                    {(category.icon && language.ltr) ? <span>{" "}</span> : null}
-                                    {category.label}
-                                    {(category.icon && !language.ltr) ? <span>{" "}</span> : null}
-                                    {(category.icon && !language.ltr) ? <i className={category.icon} /> : null}
-                                </Link>
+
+                    if (category) {
+                        return (
+                            <div className="category-tab">
+                                <div className="category-tab-box">
+                                    <Link className="category-tab-link" to={category.path}>
+                                        {(category.icon && language.ltr) ? <i className={category.icon} /> : null}
+                                        {(category.icon && language.ltr) ? <span>{" "}</span> : null}
+                                        {category.label}
+                                        {(category.icon && !language.ltr) ? <span>{" "}</span> : null}
+                                        {(category.icon && !language.ltr) ? <i className={category.icon} /> : null}
+                                    </Link>
+                                </div>
                             </div>
-                        </div>
-                    )
+                        )
+                    } else {
+                        return null
+                    }
                 }}
             </ContextConsumer>
         )
