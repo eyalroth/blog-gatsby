6a09dd88f167711db2423e4767cab28ef8872cb2
Eyal Roth <eyalroth1@gmail.com>
2019-08-11 13:26:12 +0300

    Add an actual 404 page

---
+++ src/components/NotFound/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Page from '../Page'
+import { Languages } from '../../consts/languages'
+import './style.scss'
+
+class NotFound extends React.Component {
+    render() {
+
+        return (
+            <Page subtitle="Page Not Found" languageId={Languages.English.id}>
+                <div className="notfound">
+                    <h1 className="notfound__title">
+                        Page not found :(
+                    </h1>
+                    <p className="notfound__subtitle">
+                        The link you followed may be broken, or the page may have been removed
+                    </p>
+                    <Link className="notfound__link" to="/">
+                        Go back
+                    </Link>
+                </div>
+            </Page>
+        )
+    }
+}
+
+export default NotFound
--- src/pages/404/index.md
+++
@@ -1,8 +0,0 @@
----
-title: "404"
-layout: page
-path: "/404"
-language: english
----
-
-You just hit a route that doesn't exist... the sadness.
