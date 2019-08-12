7b67e6643b0efdb8da470fd121723903bedaee7c
Eyal Roth <eyalroth1@gmail.com>
2019-08-12 11:55:33 +0300

    Add HTML metadata description to all pages

--- src/components/Page/index.jsx
+++ src/components/Page/index.jsx
@@ -8,24 +8,29 @@ class Page extends React.Component {
     render() {
         const title = Author.name[this.props.languageId]
 
-        let { subtitle } = this.props
+        let { subtitle, description } = this.props
         if (subtitle) {
             subtitle = `${subtitle} | `
         } else {
             subtitle = ""
         }
 
+        const finalTitle = `${subtitle}${title}`
+        const finalDescription = description || subtitle
+
         const language = Object.values(Languages).find(lang => lang.id ==  this.props.languageId)
 
         const helmet = (
             <Helmet 
                 key="helmet"
-                title={`${subtitle}${title}`}
                 defer={false}
                 htmlAttributes={{
                     lang: language.htmlLang
                 }}
-            />
+            >
+                <title>{finalTitle}</title>
+                <meta name="description" content={finalDescription}/>
+            </Helmet>
         )
 
         this.context.page.set(language, this.props.sidebarLinkId)
